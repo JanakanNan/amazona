e6a60add39ed52dd10a2db1383ecf606fc492ba7
j.nandakumaran@cfa-insta.fr <janakan1101@gmail.com>
2020-12-08 17:46:57 +0100

    create node server

---
+++ amazona/backend/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+        {
+            _id:'1',
+            name: 'Slim Shirt',
+            category: 'shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id:'2',
+            name: 'Fit Shirt',
+            category: 'Shirt',
+            image: '/images/d1.jpg',
+            price: 25,
+            brand: 'Nike',
+            rating: 2.2,
+            numReviews: 10
+        },
+        {
+            _id:'3',
+            name: 'Best Pants',
+            category:'Pants',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 1.5,
+            numReviews: 10
+        },
+    ]
+}
